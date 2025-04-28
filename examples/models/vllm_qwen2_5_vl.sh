f07a6788aed564820c069a43269616ee7525e218
chenyu <yu.chen.8525@gmail.com>
2025-04-28 02:20:24 +0000

    update

--- examples/models/vllm_qwen2_5_vl.sh
+++ examples/models/vllm_qwen2_5_vl.sh
@@ -13,11 +13,12 @@ export OPENAI_API_KEY="sk-proj-1234567890"
 export BASE_URL="https://api.openai.com/v1"
 
 python3 -m lmms_eval \
-    --model vllm \
-    --model_args model_version=Qwen/Qwen2.5-VL-3B-Instruct,tensor_parallel_size=1,gpu_memory_utilization=0.75 \
-    --tasks mathverse_testmini,mathvista_testmini_cot,mathvision_test,mathvision_reason_testmini \
+    --model qwen2_5_vl_vllm \
+    --model_args model_version=Qwen/Qwen2.5-VL-7B-Instruct,tensor_parallel_size=1,gpu_memory_utilization=0.8,place_visual_first=True \
+    --tasks mathverse_testmini,mathvista_testmini_cot,mathvision_test \
     --batch_size 64 \
     --log_samples \
     --log_samples_suffix vllm \
     --output_path ./logs \
-    --limit=64
+    --limit=64 \
+    --gen_kwargs "temperature=0,top_k=-1,top_p=1,max_new_tokens=8192"
