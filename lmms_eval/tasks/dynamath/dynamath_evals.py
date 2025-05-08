7c86f217eadbe5a3e164f7d4272992f1d2d6c392
yingzhepeng <yingzhepeng510@gmail.com>
2025-05-08 12:32:16 +0000

    update

--- lmms_eval/tasks/dynamath/dynamath_evals.py
+++ lmms_eval/tasks/dynamath/dynamath_evals.py
@@ -1,38 +1,42 @@
+import json
 import os
 import re
 import time
 from pathlib import Path
+
 import numpy as np
+import pandas as pd
 import requests
 import yaml
-import json
-import pandas as pd
 from Levenshtein import distance
 from loguru import logger as eval_logger
 from openai import AzureOpenAI, OpenAI
 
+
 def preprocess(str1):
     str1 = str(str1)
     if 0 <= str1.find("{") < str1.rfind("}"):
-        str1 = str1[str1.find("{"): str1.rfind("}") + 1]
+        str1 = str1[str1.find("{") : str1.rfind("}") + 1]
     str2 = str1.replace("\\", "")
     str2 = str2.replace("\\n", "\n")
     return str2
 
+
 def transfer(str1):
     if "\u03c0" in str1:
-        strs = str1.split('\u03c0')
+        strs = str1.split("\u03c0")
         str1 = strs[0]
         return float(str1) * np.pi
     else:
         return float(str1)
-    
+
+
 def parse_answer(answer, answer_type="multiple choice"):
     if answer_type == "float":
         if answer.isdigit():
             return True, float(answer)
         else:
-            parts = answer.split(' ')
+            parts = answer.split(" ")
             answer = parts[0]
             try:
                 answer = transfer(answer)
@@ -43,14 +47,15 @@ def parse_answer(answer, answer_type="multiple choice"):
         if len(answer) == 1:
             return True, answer.upper()
         else:
-            in_flag = [ch in answer.upper() for ch in 'ABCDE']
+            in_flag = [ch in answer.upper() for ch in "ABCDE"]
             if sum(in_flag) == 1:
-                for ch in 'ABCDE':
+                for ch in "ABCDE":
                     if ch in answer.upper():
                         return True, ch
             return False, None
     else:
         return True, answer
+
 
 class DynaMathEvaluator:
     API_TYPE = os.getenv("API_TYPE", "openai")
@@ -136,37 +141,34 @@ class DynaMathEvaluator:
         result = {}
         pred = preprocess(prediction)
         succeed, short_answer = None, None
-
+        result["gpt_extract_answer"] = ""
         try:
             dj = json.loads(pred, strict=False)
             short_answer = dj.get("short answer")
             assert short_answer is not None
-            succeed, short_answer = parse_answer(short_answer, answer_type=doc['answer_type'])
+            succeed, short_answer = parse_answer(short_answer, answer_type=doc["answer_type"])
             assert succeed
         except:
             # Failed to parse the JSON, use an auxiliary LLM to get the short answer
-            if doc['answer_type'] == 'multiple choice':
+            if doc["answer_type"] == "multiple choice":
                 inst = "Output the corresponing choice option, such as 'A', 'B', 'C', 'D', in a single line."
-            elif doc['answer_type'] == 'float':
+            elif doc["answer_type"] == "float":
                 inst = "Output a three-digit floating-point number in a single line."
             else:
-                inst = (
-                    "Output a short answer in a single line. Any float numbers in the answer "
-                    "should be formatted as three-digit floating-point numbers."
-                )
+                inst = "Output a short answer in a single line. Any float numbers in the answer " "should be formatted as three-digit floating-point numbers."
 
             prompt = f"Free-form answer: {pred}\nInstruction: {inst}"
             response = pred
-            succeed, short_answer = parse_answer(response, doc['answer_type'])
+            succeed, short_answer = parse_answer(response, doc["answer_type"])
 
             if not succeed:
-                print("using api to get the answer...")
                 response = self.get_chat_response(prompt)
-                succeed, short_answer = parse_answer(response, doc['answer_type'])
-        
-        if doc['answer_type'] == 'float':
+                result["gpt_extract_answer"] = response
+                succeed, short_answer = parse_answer(response, doc["answer_type"])
+
+        if doc["answer_type"] == "float":
             if succeed:
-                diff = float(short_answer) - float(doc['ground_truth'])
+                diff = float(short_answer) - float(doc["ground_truth"])
                 result["extraction_prediction"] = short_answer
                 if abs(diff) <= 0.001:
                     result["true_false"] = True
@@ -177,13 +179,13 @@ class DynaMathEvaluator:
                 result["extraction_prediction"] = None
                 result["true_false"] = False
                 return result
-        elif doc['answer_type'] == 'multiple choice':
+        elif doc["answer_type"] == "multiple choice":
             if succeed:
                 result["extraction_prediction"] = short_answer
-                result["true_false"] = (short_answer == doc['ground_truth'])
+                result["true_false"] = short_answer == doc["ground_truth"]
                 return result
             else:
-                if doc['ground_truth'] in pred[:3].upper():
+                if doc["ground_truth"] in pred[:3].upper():
                     result["extraction_prediction"] = None
                     result["true_false"] = True
                 else:
@@ -193,40 +195,41 @@ class DynaMathEvaluator:
         else:
             if succeed:
                 result["extraction_prediction"] = short_answer
-                result["true_false"] = (short_answer.lower() in doc['ground_truth'].lower())
+                result["true_false"] = short_answer.lower() in doc["ground_truth"].lower()
                 return result
             else:
                 result["extraction_prediction"] = None
-                result["true_false"] = (short_answer.lower() in doc['ground_truth'].lower())
+                result["true_false"] = short_answer.lower() in doc["ground_truth"].lower()
                 return result
-    
+
     def eval_results(self, results):
         results_df = pd.DataFrame(results)
         score_avg = {}
         # 1. 计算overall_accuracy
         score_avg["overall_accuracy"] = results_df["true_false"].mean()
         # 2. 分科目统计，统计各个科目的 overaccuracy
-        subs = set(results_df['subject'])
+        subs = set(results_df["subject"])
         for sub in subs:
-            sub_df = results_df[results_df['subject'] == sub]
-            score_avg[f'Subject-{sub}'] = sub_df["true_false"].mean()
+            sub_df = results_df[results_df["subject"] == sub]
+            score_avg[f"Subject-{sub}"] = sub_df["true_false"].mean()
 
         # 3. 分知识水平统计，统计各个知识水平的 overaccuracy
-        lvls = set(results_df['knowledge_level'])
+        lvls = set(results_df["knowledge_level"])
         for lvl in lvls:
-            knowledge_level_df = results_df[results_df['knowledge_level'] == lvl]
-            score_avg[f'Level-{lvl}'] = knowledge_level_df["true_false"].mean()
+            knowledge_level_df = results_df[results_df["knowledge_level"] == lvl]
+            score_avg[f"Level-{lvl}"] = knowledge_level_df["true_false"].mean()
 
         # 创建一个字典，表示平均情况
-        score = {'Setting': 'Average'}  # 添加一个标识列
-        score.update(score_avg)         # 将平均分数添加到字典中
+        score = {"Setting": "Average"}  # 添加一个标识列
+        score.update(score_avg)  # 将平均分数添加到字典中
 
         # results_df to json
         results = results_df.to_dict(orient="records")
         print(score)
         return results, score
 
-'''print(score):
+
+"""print(score):
     {'Setting': 'Average', 
     'overall_accuracy': 0.59375,
     'Subject-plane geometry': 0.7272727272727273,
@@ -238,4 +241,4 @@ class DynaMathEvaluator:
     'Level-high school': 0.5294117647058824,
     'Level-elementary school': 0.6666666666666666
     }
-'''
+"""
--- lmms_eval/tasks/dynamath/utils.py
+++ lmms_eval/tasks/dynamath/utils.py
@@ -1,7 +1,8 @@
 import json
 import os
-from pathlib import Path
 import time
+from pathlib import Path
+
 import pandas as pd
 import yaml
 from loguru import logger as eval_logger
@@ -15,6 +16,7 @@ dynamath_evaluator = DynaMathEvaluator(api_key=os.getenv("OPENAI_API_KEY", "YOUR
 def get_timestamp():
     nowtime = time.strftime("%Y%m%d-%H%M", time.localtime(time.time()))
     return nowtime
+
 
 with open(Path(__file__).parent / "dynamath.yaml", "r") as f:
     raw_data = f.readlines()
@@ -40,24 +42,22 @@ detailed steps needed to solve the question, and the 'short answer' key should p
 Example of expected JSON response format:
 
 """
-    EXAMPLE = {
-        "solution": "[Detailed step-by-step explanation]",
-        "short answer": "[Concise Answer]"
-    }
+    EXAMPLE = {"solution": "[Detailed step-by-step explanation]", "short answer": "[Concise Answer]"}
     TEXT_EXAMPLE = json.dumps(EXAMPLE, indent=4)
     query_prompt = f"## Question\n {doc['question']}"
-    if doc['answer_type'] == 'multiple choice':
+    if doc["answer_type"] == "multiple choice":
         inst = "Provide the corresponing choice option in the 'short answer' key, such as 'A', 'B', 'C', or 'D'."
-    elif doc['answer_type'] == 'float':
+    elif doc["answer_type"] == "float":
         inst = "Format the answer as a three-digit floating-point number and provide it in the 'short answer' key."
     else:
         inst = "Float numbers in the answer should be formatted as three-digit floating-point numbers."
-    
-    query_prompt += GUIDE.format(INST=inst) + TEXT_EXAMPLE
+    if lmms_eval_specific_kwargs is not None:
+        if lmms_eval_specific_kwargs.get("use_guide_prompt", False):
+            query_prompt = query_prompt + GUIDE.format(INST=inst) + TEXT_EXAMPLE
     return query_prompt
 
 
-# process 里面判断样本的answer和prediction是不是一致的    
+# process 里面判断样本的answer和prediction是不是一致的
 def dynamath_process_results(doc, results):
     prediction = results[0].strip()
     result = dynamath_evaluator.judge_answer(prediction, doc)
@@ -74,9 +74,11 @@ def dynamath_process_results(doc, results):
             "id": doc["id"],
             "varid": doc["varid"],
             "qid": doc["qid"],
+            "gpt_extract_answer": result["gpt_extract_answer"],
             "index": doc["index"],
         }
     }
+
 
 # aggregate里面计算总的指标
 def dynamath_aggregate_results_eval(results, args):
@@ -94,7 +96,6 @@ def dynamath_aggregate_results_eval(results, args):
         json.dump(scores, f, indent=4)
 
     eval_logger.info(f"Saved scores to {path}")
-    if scores['overall_accuracy'] == 0:
-        return None 
-    return scores['overall_accuracy']
-
+    if scores["overall_accuracy"] == 0:
+        return None
+    return scores["overall_accuracy"]
